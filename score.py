ed9b4592a5d0a14438377975377d4b24d88e7001
Dos4a <d.rozakhun@gmail.com>
2025-02-09 21:00:43 +0800

    Separate some code into the funcions and added random score

--- score.py
+++ score.py
@@ -1,18 +1,20 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+import random
 
-# TODO: Fix this!
+def main():
+    user_score = float(input("Enter score: "))
+    print(determine_result(user_score))
+    random_score = random.randint(0, 100)
+    print(f"Random score: {random_score}")
+    print(determine_result(random_score))
 
-score = float(input("Enter score: "))
+def determine_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
 
-if score < 0 or score > 100:
-    print('Invalid score')
-elif score > 90:
-    print('Excellent')
-elif score > 50:
-    print('Passable')
-else:
-    print('Bad')
-
+main()
